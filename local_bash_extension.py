b2de7ffe7567d58945b5f25d20faef1522c15883
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-11-22 21:01:44 -0800

    local execuiton works

--- local_bash_extension.py
+++ local_bash_extension.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dotenv import load_dotenv
 from jess_extension import jess_extension
@@ -11,6 +12,7 @@ GCP_PROJECT_FOR_PUBSUB = os.getenv('GCP_PROJECT_FOR_PUBSUB')
 GCP_PUBSUB_SUBSCRIPTION_FROM_HOST = os.getenv('GCP_PUBSUB_SUBSCRIPTION_FROM_HOST')
 GCP_PUBSUB_TOPIC_TO_HOST = os.getenv('GCP_PUBSUB_TOPIC_TO_HOST')
 
+initiated = False
 
 def publish_command(project_id, topic_id, command):
     """Publishes a command to the specified Pub/Sub topic"""
@@ -21,31 +23,26 @@ def publish_command(project_id, topic_id, command):
     return future.result()
 
 def subscribe_to_result(project_id, subscription_id, timeout=None):
-    """Subscribes to the specified Pub/Sub subscription for results"""
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_id)
+    result = {
+        "text": ""
+    }
+    global initiated
+    if not initiated:
+        initiated = True
+        subscriber = pubsub_v1.SubscriberClient()
+        subscription_path = subscriber.subscription_path(project_id, subscription_id)
+        def callback(message):
+            print(f"Received result: {message.data.decode('utf-8')}")
+            result["text"] = message.data.decode('utf-8')
+            message.ack()
 
-    def callback(message):
-        print(f"Received result: {message.data.decode('utf-8')}")
-        callback.txt = message.data.decode('utf-8')
-        message.ack()
-        # Close the subscriber client to stop its background thread
-        # try:
-        #     subscriber.close()
-        # except Exception as e:
-        #     print(f"An error occurred: {e}")
-
-    subscription_future = subscriber.subscribe(subscription_path, callback=callback)
-    print(f"Listening for results on {subscription_path}...")
-
-    try:
-        # Wait for the subscriber to complete, or for a timeout
-        subscription_future.result(timeout=timeout)
-    except TimeoutError:
-        # Handle the timeout case as needed
-        print("No response received within the timeout period.")
-        subscription_future.cancel()
-    return callback.txt
+        subscriber.subscribe(subscription_path, callback=callback)
+        print(f"Listening for results on {subscription_path}...")
+    while result["text"] == "":
+        time.sleep(1)
+    to_return = result["text"]
+    result["text"] = ""
+    return to_return
 
 @jess_extension(
     description="Send bash command on execution on a user's host",
@@ -58,4 +55,4 @@ def send_bash_command_to_local_host(command: str):
     publish_command(GCP_PROJECT_FOR_PUBSUB, GCP_PUBSUB_TOPIC_TO_HOST, command)
 
     # Subscribe to the result and wait for the response
-    subscribe_to_result(GCP_PROJECT_FOR_PUBSUB, GCP_PUBSUB_SUBSCRIPTION_FROM_HOST, timeout=20) 
+    return subscribe_to_result(GCP_PROJECT_FOR_PUBSUB, GCP_PUBSUB_SUBSCRIPTION_FROM_HOST, timeout=20) 
